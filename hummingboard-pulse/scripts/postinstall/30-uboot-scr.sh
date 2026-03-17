59cb52e35625a83472aeee0bc5ede00a1073f3d2
Valentin Geffroy <valentin.geffroy@iot.bzh>
2026-03-17 10:59:29 +0100

    Align bootscript for HummingBoard Pulse
    Signed-off-by: Valentin Geffroy <valentin.geffroy@iot.bzh>

--- hummingboard-pulse/scripts/postinstall/30-uboot-scr.sh
+++ hummingboard-pulse/scripts/postinstall/30-uboot-scr.sh
@@ -4,12 +4,10 @@ set -e
 
 echo "Create boot.scr needed by uboot in $BUILDROOT/boot/bootscript.txt ..."
 cat <<'EOF' > $BUILDROOT/boot/bootscript.txt
-setenv mmcroot /dev/mmcblk1p3 rootwait rw
-setenv loadaddr 0x40400000
-setenv fdt_addr_r 0x43000000
-setenv bootargs console=ttymxc1,115200 root=${mmcroot} security=smack nohz_full=2 irqaffinity=0-1,3 rcu_nocbs=2 rcu_nocb_poll nosoftlockup
-load mmc 1:2 ${loadaddr} Image
-load mmc 1:2 ${fdt_addr_r} imx8mp-hummingboard-pulse.dtb
+setenv mmcroot /dev/mmcblk${devnum}p3 rootwait rw
+setenv bootargs ${jh_clk} console=${console} root=${mmcroot} security=smack
+load mmc ${devnum}:${mmcpart} ${loadaddr} Image
+load mmc ${devnum}:${mmcpart}  ${fdt_addr_r} imx8mp-hummingboard-pulse.dtb
 booti ${loadaddr} - ${fdt_addr_r}
 EOF
 
